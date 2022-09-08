bed3d7c7b3300759f72dc7e9556148f88265a873
srilekhareddy02 <100460960+srilekhareddy02@users.noreply.github.com>
2022-09-08 22:44:03 +0530

    Add files via upload

--- cluster.py
+++ cluster.py
@@ -16,6 +16,7 @@ st.set_page_config(page_title = 'Customer Behaviour Analysis Web App', layout='c
 st.title('Customer Behaviour Analysis')
 st.image("""https://t4.ftcdn.net/jpg/02/97/85/17/360_F_297851731_apSBHOhfsy62vf9X1CGpH1FcvGHtsHEI.jpg""")
 st.sidebar.image("vertical.jpg",use_column_width=True)
+st.sidebar.image("""https://media.istockphoto.com/photos/bar-graph-moving-up-on-digital-display-stock-market-and-finance-picture-id1219273018?s=612x612""")
 # customer segmentation function
 def segment_customers(input_data):
     
